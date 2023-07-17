e86eee84ee458db6dc028febf95f73b7236bdaec
BiplawSingh <biplawgaurav@gmail.com>
2023-07-17 21:38:41 +0530

    Completed city repo

--- src/repository/city_repository.js
+++ src/repository/city_repository.js
@@ -3,10 +3,14 @@ const { City } = require('../models/index');
 class CityRepository{
     // since we're going to receive objects in the function, we're restructuring it then and there 
     async createCity({ name }){
+        //here the mapping to the name key-value pair will be done implicity
         try{
-            const city = await City.create({ name });
+            const city = await City.create({ 
+                name
+            });
             return city;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
@@ -18,7 +22,33 @@ class CityRepository{
                 id: cityId
               }  
             });
+            return true;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    //first we need to fetch exactly what data we want to update
+    async updateCity(cityId, data){
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+        } catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
