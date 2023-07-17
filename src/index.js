001f149617d14dffc30cb824ed01540e268dd651
BiplawSingh <biplawgaurav@gmail.com>
2023-07-18 02:43:41 +0530

    Added routing for city

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
 
 
 const setupAndStartServer = async () => {
@@ -10,7 +11,9 @@ const setupAndStartServer = async () => {
     const app = express();
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', ApiRoutes);
     
 
     app.listen(PORT, () => {
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -32,11 +32,9 @@ class CityRepository{
     //first we need to fetch exactly what data we want to update and what city
     async updateCity(cityId, data){
         try {
-            const city = await City.update(data, {
-                where: {
-                    id: cityId
-                }
-            });
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         } catch(error){
             console.log("Something went wrong in the repository layer");
