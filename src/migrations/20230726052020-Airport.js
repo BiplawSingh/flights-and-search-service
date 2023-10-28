006c5db3f5e8a4b6616d09d6c852558b0bf553f0
BiplawSingh <biplawgaurav@gmail.com>
2023-10-29 00:07:10 +0530

    Added flights model

--- src/migrations/20230726052020-Airport.js
+++ src/migrations/20230726052020-Airport.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       name: {
         type: Sequelize.STRING,
-        alloNull: false
+        allowNull: false
       },
       address: {
         type: Sequelize.STRING
